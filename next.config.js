cd608bc1b520d54efcf4d2b5019711a148405d9b
Variable.K <variable.k@proton.me>
2025-12-09 21:59:13 -0500

    Enable Turbopack with empty config as suggested by Next.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: false,
+  turbopack: {},
   serverExternalPackages: ['@xmtp/xmtp-js', '@xmtp/proto'],
-  webpack: (config, { isServer }) => {
-    // Externalize problematic packages
-    config.externals = config.externals || [];
-    config.externals.push({
-      'thread-stream': 'commonjs thread-stream',
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
