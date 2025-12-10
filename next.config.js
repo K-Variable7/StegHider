19caf6a923386c4d9820f2a84d2045c805f83150
Variable.K <variable.k@proton.me>
2025-12-09 21:47:08 -0500

    Add webpack rules to exclude problematic test files from node_modules

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   outputFileTracingRoot: require('path').join(__dirname, '../../'),
   serverExternalPackages: ['@xmtp/xmtp-js', '@xmtp/proto'],
+  webpack: (config, { isServer }) => {
+    // Exclude test files from node_modules
+    config.module.rules.push({
+      test: /\.test\.(js|ts|tsx|mjs)$/,
+      include: /node_modules/,
+      use: 'ignore-loader'
+    });
+
+    // Exclude specific problematic files
+    config.module.rules.push({
+      test: /thread-stream\/test\/.*\.(js|ts|tsx|mjs|md|zip|sh)$/,
+      use: 'ignore-loader'
+    });
+
+    // Exclude README and LICENSE files in thread-stream
+    config.module.rules.push({
+      test: /thread-stream\/(README\.md|LICENSE)$/,
+      use: 'ignore-loader'
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
