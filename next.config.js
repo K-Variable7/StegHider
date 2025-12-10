e26ef42949d7efe25a451151ec079d703e982b2a
Variable.K <variable.k@proton.me>
2025-12-09 21:56:16 -0500

    Externalize thread-stream package to prevent bundling test files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   turbopack: false,
   serverExternalPackages: ['@xmtp/xmtp-js', '@xmtp/proto'],
+  webpack: (config, { isServer }) => {
+    // Externalize problematic packages
+    config.externals = config.externals || [];
+    config.externals.push({
+      'thread-stream': 'commonjs thread-stream',
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
