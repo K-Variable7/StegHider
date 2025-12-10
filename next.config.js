1ae782d10be16ae663dec452da6e6a12da2a0d87
Variable.K <variable.k@proton.me>
2025-12-09 22:02:21 -0500

    Use webpack with resolve aliases to ignore problematic dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {},
+  turbopack: false,
   serverExternalPackages: ['@xmtp/xmtp-js', '@xmtp/proto'],
+  webpack: (config, { isServer }) => {
+    // Resolve aliases for missing dependencies
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'pino-elasticsearch': false,
+      'tap': false,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
